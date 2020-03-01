0978414527a5c1a3225a534a1844d3889678307f
xudaye2001 <git@github.com:xudaye2001/security-in-action.git>
2020-03-01 08:35:03 +0800

    first commit

---
+++ src/main/java/com/bookindle/boosystem/BoosystemApplication.java
@@ -0,0 +1,13 @@
+package com.bookindle.boosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoosystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoosystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookindle/boosystem/BoosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookindle.boosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoosystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
